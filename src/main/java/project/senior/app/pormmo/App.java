ab87b894832b14768ff86d4f73c2be29dc3dbc03
PorMMo <pormmo@gmail.com>
2012-11-10 17:37:47 -0600

    Adjusted jna library piece to only fire for os.name startswith Windows

--- src/main/java/project/senior/app/pormmo/App.java
+++ src/main/java/project/senior/app/pormmo/App.java
@@ -2,11 +2,14 @@ package project.senior.app.pormmo;
 
 import com.sun.jna.NativeLibrary;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        NativeLibrary.addSearchPath("libvlc", "C:\\Program Files\\VideoLAN\\VLC");
+public class App {
+
+    public static void main(String[] args) {
+        if (System.getProperty("os.name").startsWith("Windows")) 
+        {
+            NativeLibrary.addSearchPath("libvlc", "C:\\Program Files\\VideoLAN\\VLC");
+        }
+        
         UserInterfaceFrame uIF = new UserInterfaceFrame();
     }
 }
