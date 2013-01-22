371fa40941eeb4b414ae7c955ef4ac50e12f7054
PorMMo <pormmo@gmail.com>
2013-01-21 22:26:38 -0600

    Attempt to merge stash with upstream

--- src/main/java/project/senior/app/pormmo/UserInterfaceFrame.java
+++ src/main/java/project/senior/app/pormmo/UserInterfaceFrame.java
@@ -1,9 +1,13 @@
 package project.senior.app.pormmo;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.ScrollPane;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -17,6 +21,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 
 /**
  * @author John Fisher
@@ -24,105 +29,229 @@ import javax.swing.JPanel;
 public class UserInterfaceFrame extends JFrame
 {
 
-    private JPanel controlPanel;
-    private OutputPanel outputPanel;
-    private File selectedInputFile;
-    private GridBagLayout gl;
-    private VlcInterface vlcIFace;
+  private JPanel controlPanel;
+  private OutputPanel outputPanel;
+  private File selectedInputFile;
+  private GridBagLayout controlLayout;
+  private VlcInterface vlcIFace;
+  private GridBagConstraints controlGBC;
+  private ScrollPane sPane;
+  private JFrame me;
+  private BufferedImage snapshot;
 
-    public UserInterfaceFrame()
-    {
-        setPreferredSize(new Dimension(500, 500));
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        GridBagConstraints c = new GridBagConstraints();
-            c.fill = GridBagConstraints.HORIZONTAL;
-        gl = new GridBagLayout();
-        setLayout(gl);
-        
-        JMenuBar frameMenuBar = new JMenuBar();
-        JMenu fileMenu = new JMenu("File");
-        JMenuItem openMenuItem = new JMenuItem("Open");
-        openMenuItem.addMouseListener(new UserInterfaceFrameMenuListener());
-        fileMenu.add(openMenuItem);
-        JMenuItem exitMenuItem = new JMenuItem("Exit");
-        exitMenuItem.addMouseListener(new UserInterfaceFrameMenuListener());
-        fileMenu.add(exitMenuItem);
+  public UserInterfaceFrame()
+  {
+    initFrame();
+    initLayouts();
+    initMenu();
+    initControlPanel();
+    initOutputPanel();
+    initDisplay();
+    showFrame();
+  }
 
-        frameMenuBar.add(fileMenu);
+  private void initFrame()
+  {
+    setPreferredSize(new Dimension(500, 500));
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLayout(new BorderLayout());
+    this.addComponentListener(new FrameListener());
+    me = this;
+  }
+  
+  private void showFrame()
+  {
+    pack();
+    setVisible(true);
+  }
+  
+  private void initLayouts()
+  {
+    controlLayout = new GridBagLayout();    
+    controlGBC = new GridBagConstraints();
+    controlGBC.fill = GridBagConstraints.HORIZONTAL;
 
-        setJMenuBar(frameMenuBar);
+  }
 
-        outputPanel = new OutputPanel();
-            outputPanel.setBackground(Color.green);
-            outputPanel.setPreferredSize(new Dimension(500,500));
-            
-        controlPanel = new JPanel();
-            controlPanel.setBackground(Color.blue);
-            
-        JButton playButton = new JButton("Play");
-            playButton.addMouseListener(new MediaControlsListener());
-            controlPanel.add(playButton);
-        JButton stopButton = new JButton("Stop");
-            stopButton.addMouseListener(new MediaControlsListener());
-            controlPanel.add(stopButton);
-        JButton pauseButton = new JButton("Pause");
-            pauseButton.addMouseListener(new MediaControlsListener());
-            controlPanel.add(pauseButton);
-        JButton snapshotButton = new JButton("Snapshot");
-            snapshotButton.addMouseListener(new MediaControlsListener());
-            controlPanel.add(snapshotButton);
-        
-        
-        c.gridx = 0;
-        c.gridy = 0;
-        add(outputPanel, c);
+  private void initDisplay()
+  {
+    sPane = new ScrollPane();
+    sPane.add(outputPanel);
+    sPane.setPreferredSize(this.getPreferredSize());
+    
+    add(controlPanel, BorderLayout.NORTH);
+    add(sPane, BorderLayout.CENTER);
+  }
 
-        c.gridx = 0;
-        c.gridy = 1;
-        add(controlPanel, c);
-        
-        pack();
-        setVisible(true);
-    }
+  private void initMenu()
+  {
+    JMenuBar frameMenuBar = new JMenuBar();
+    JMenu fileMenu = new JMenu("File");
+    JMenuItem openMenuItem = new JMenuItem("Open");
+    openMenuItem.addMouseListener(new UserInterfaceFrameMenuListener());
+    fileMenu.add(openMenuItem);
+    JMenuItem exitMenuItem = new JMenuItem("Exit");
+    exitMenuItem.addMouseListener(new UserInterfaceFrameMenuListener());
+    fileMenu.add(exitMenuItem);
 
-    private void showFileSelect()
-    {
-        JFileChooser jFC = new JFileChooser();
-        jFC.setAcceptAllFileFilterUsed(false);
-        jFC.setFileFilter(new ExtFileFilter(new String[]{".mp4",".mpg",".avi",".wmv"}));
-        jFC.setFileFilter(new ExtFileFilter(new String[]{".png",".jpg"}));       
+    frameMenuBar.add(fileMenu);
 
-        jFC.showDialog(this, "Open");
-        selectedInputFile = jFC.getSelectedFile();  
-        OpenSource();
-    }
+    setJMenuBar(frameMenuBar);
+  }
 
-    private void OpenSource()
-    {
-        if(selectedInputFile.canRead())
-        {
-            try {
-             vlcIFace = new VlcInterface(selectedInputFile);
-            }
-            catch (java.lang.NoClassDefFoundError e)
+  private void initOutputPanel()
+  {
+    outputPanel = new OutputPanel();
+    outputPanel.setPreferredSize(new Dimension(500, 500));
+  }
+
+  private void initControlPanel()
+  {
+
+    controlPanel = new JPanel();
+    controlPanel.setLayout(controlLayout);
+
+    JButton playButton = new JButton("Play");
+    playButton.addMouseListener(new MediaControlsListener());
+    controlGBC.gridx = 0;
+    controlGBC.gridy = 0;
+    controlPanel.add(playButton, controlGBC);
+
+    JButton stopButton = new JButton("Stop");
+    stopButton.addMouseListener(new MediaControlsListener());
+    controlGBC.gridx = 1;
+    controlGBC.gridy = 0;
+    controlPanel.add(stopButton, controlGBC);
+    
+    JButton fwdButton = new JButton("Forward");
+    fwdButton.addMouseListener(new MediaControlsListener());
+    controlGBC.gridx = 2;
+    controlGBC.gridy = 0;
+    controlPanel.add(fwdButton, controlGBC);
+
+    JButton rwdButton = new JButton("Rewind");
+    rwdButton.addMouseListener(new MediaControlsListener());
+    controlGBC.gridx = 3;
+    controlGBC.gridy = 0;
+    controlPanel.add(rwdButton, controlGBC);
+    
+    JButton pauseButton = new JButton("Pause");
+    pauseButton.addMouseListener(new MediaControlsListener());
+    controlGBC.gridx = 4;
+    controlGBC.gridy = 0;
+    controlPanel.add(pauseButton, controlGBC);
+    
+    JButton snapshotButton = new JButton("Snapshot");
+    snapshotButton.addMouseListener(new MediaControlsListener());
+    controlGBC.gridx = 5;
+    controlGBC.gridy = 0;
+    controlPanel.add(snapshotButton, controlGBC);
+
+    JSlider posSlider = new JSlider();
+    controlGBC.gridx = 0;
+    controlGBC.gridy = 1;
+    controlGBC.gridwidth = 6;
+    controlPanel.add(posSlider, controlGBC);
+  }
+
+  private void showFileSelect()
+  {
+    JFileChooser jFC = new JFileChooser();
+    jFC.setAcceptAllFileFilterUsed(false);
+    jFC.setFileFilter(new ExtFileFilter(new String[]
             {
-                System.out.println("Error: " + e.getMessage());
-            }
-        }
+              ".png", ".jpg"
+            }));
+    jFC.setFileFilter(new ExtFileFilter(new String[]
+            {
+              ".mp4", ".mpg", ".avi", ".wmv"
+            }));
+
+    jFC.showDialog(this, "Open");
+    selectedInputFile = jFC.getSelectedFile();
+    OpenSource();
+  }
+
+  private void OpenSource()
+  {
+    if (selectedInputFile.canRead())
+    {
+      try
+      {
+        vlcIFace = new VlcInterface(selectedInputFile);
+        
+      } catch (java.lang.NoClassDefFoundError e)
+      {
+        System.out.println("Error: " + e.getMessage());
+      }
+    }
+  }
+
+  private class MediaControlsListener implements MouseListener
+  {
+
+    @Override
+    public void mouseClicked(MouseEvent e)
+    {
     }
 
-    private class MediaControlsListener implements MouseListener{
+    @Override
+    public void mousePressed(MouseEvent e)
+    {
+    }
 
-        @Override
-        public void mouseClicked(MouseEvent e)
-        {
-        }
+    @Override
+    public void mouseReleased(MouseEvent e)
+    {
 
-        @Override
-        public void mousePressed(MouseEvent e)
+      if (vlcIFace == null)
+      {
+        JOptionPane.showMessageDialog(null, "Please choose a file via the File menu");
+      } else
+      {
+        JButton clickedButton = (JButton) e.getSource();
+
+        switch (clickedButton.getText().toLowerCase())
         {
+          case "play":
+            vlcIFace.Play();
+            break;
+          case "forward":
+            vlcIFace.Forward();
+            break;
+          case "rewind":
+            vlcIFace.Rewind();
+            break;
+          case "stop":
+            vlcIFace.Stop();
+            break;
+          case "pause":
+            vlcIFace.Pause();
+            break;
+          case "snapshot":
+            vlcIFace.Snapshot();
+            snapshot = vlcIFace.LastSnapShot();
+
+            outputPanel.DrawBufferedImage(snapshot);
+            outputPanel.setPreferredSize(new Dimension(me.getWidth(), me.getHeight()-controlPanel.getHeight()));
+            break;
         }
+      }
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e)
+    {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e)
+    {
+    }
+  }
+
+  private class UserInterfaceFrameMenuListener extends MouseAdapter
+  {
 
         @Override
         public void mouseReleased(MouseEvent e)
@@ -164,34 +293,32 @@ public class UserInterfaceFrame extends JFrame
             }
         }
 
-        @Override
-        public void mouseEntered(MouseEvent e)
-        {
-        }
+    @Override
+    public void componentResized(ComponentEvent e)
+    {
+      controlPanel.setPreferredSize(new Dimension(me.getWidth(), controlPanel.getHeight()));
+      outputPanel.setPreferredSize(new Dimension(me.getWidth(), outputPanel.getHeight()-controlPanel.getHeight()));
+      me.validate();
+      me.repaint();
+    }
 
-        @Override
-        public void mouseExited(MouseEvent e)
-        {
-        }
-        
+    @Override
+    public void componentMoved(ComponentEvent e)
+    {
+      System.out.println("Comp Mvd");
+    }
+
+    @Override
+    public void componentShown(ComponentEvent e)
+    {
+      System.out.println("Comp Shwn");
+    }
+
+    @Override
+    public void componentHidden(ComponentEvent e)
+    {
+      System.out.println("Comp Hid");    
     }
     
-    private class UserInterfaceFrameMenuListener extends MouseAdapter
-    {
-
-        @Override
-        public void mouseReleased(MouseEvent e)
-        {
-            JMenuItem jMenuItem = (JMenuItem) e.getSource();
-            switch (jMenuItem.getText().toLowerCase())
-            {
-                case "open":
-                    showFileSelect();
-                    break;
-                case "exit":
-                    System.exit(0);
-                    break;
-            }
-        }
-    }
+  }
 }
